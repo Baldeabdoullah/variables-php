83ff805f0a5d30a4c7de23feac70da38c82c1c94
Aboullah Balde <abdoullahbalde@gmail.com>
2023-08-05 17:58:21 +0200

    variables en PHP

---
+++ 02_variables.php
@@ -0,0 +1,43 @@
+<?php
+$nom = 'Duplex';
+$prenom = 'kamdjou';
+$nickName = 'tony';
+$age = 32;
+$account_balance = 1.2;
+$est_marie = true;
+
+echo $account_balance;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<h1>
+    <?php
+    echo "je suis  $nom  $nickName ";
+    ?>
+</h1>
+
+
+<p></p>
+
+
+<h2>
+    <?php
+    echo  $prenom;
+    ?>
+
+</h2>
+
+<body>
+
+</body>
+
+</html>
